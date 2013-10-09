9e6156fce471f67fb9302c671f61aac3f201f4e5
Kenny Do <kedo@ocf.berkeley.edu>
2013-10-09 02:43:45 -0700

    working on getting a simple black png to work

---
+++ test/blackpng.cpp
@@ -0,0 +1,12 @@
+#include "film.h"
+
+/*
+ *
+ * I tried to compile this with:
+ *  g++ -g -Wall -I include/ src/film.cpp test/blackpng.cpp src/primitives.cpp
+ */
+int main (int argc, char* argv[]) {
+    Film* f = new Film(640, 480);
+
+    f->write((char*) "poop.png");
+}
